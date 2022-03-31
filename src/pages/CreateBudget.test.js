b87d9e054bd939a1d6cc460fc10947c425d3d77b
repositorio269 <repositorio269@gmail.com>
2022-03-31 18:17:42 +0200

    all credits to Raul

--- src/pages/CreateBudget.test.js
+++ src/pages/CreateBudget.test.js
@@ -1,19 +1,19 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import CreateBudget from "./CreateBudget";
 
-describe('Formulario crear cliente', () => { // Describe el suite test
+describe('Formulario crear cliente',  () => { // Describe el suite test
 
-    test('Valida el campo CIF', () => { // Decribe el case test
-        // render(<CreateBudget />); // si recibiera props se las pasaremos de datos mock
+    test('Valida el campo CIF', async () => { // Decribe el case test
+        render(<CreateBudget />); // si recibiera props se las pasaremos de datos mock
         // En el caso de necesitar ver el componente renderizado una fórmula
         // es pasarlo a una constante y invocar su metodo debug
-        const component = render(<CreateBudget />);
+        // const component = render(<CreateBudget />);
         // component.debug();
-        // let validationMessage;
-        // let textInput;
-        const validationMessage = component.findByTestId('test-cif-message');
-        const textInput = component.findByTestId('test-cif-input');
+        let validationMessage;
+        let textInput;
+        await screen.findByTestId('test-cif-message').then(data => validationMessage = data);
+        await screen.findByTestId('test-cif-input').then(data => textInput = data);
         userEvent.type(textInput, 'Z');
         expect(validationMessage).toHaveTextContent('El CIF debe comenzar por letra válida y tener 9 caracteres');
     })
